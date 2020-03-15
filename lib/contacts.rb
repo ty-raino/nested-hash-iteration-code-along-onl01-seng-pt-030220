5cfef6dcced8170bd3a844d9079f06d94c0b90f7
Ty Raino <mstymetris@yahoo.com>
2020-03-15 21:40:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  if person == "Jon Snow"
+  if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
     end
